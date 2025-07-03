fc7a4d0e317fce2e8eea583b94eec2eb31fb1e70
Manuel Hernández <manuel.hdez951@gmail.com>
2025-07-02 23:51:49 -0600

    App MVVM

---
+++ GestorTareas/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+
+namespace GestorTareas.ViewModels
+{
+   public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int count;
+
+        [RelayCommand]
+        public void Sumar()
+        {
+            Count++;
+        }
+    }
+}
--- GestorTareas/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/GestorTareas.AssemblyInfo.cs
+++ GestorTareas/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/GestorTareas.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorTareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fc9db27c3f0174541006224d105093b3f34e4964")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorTareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorTareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
